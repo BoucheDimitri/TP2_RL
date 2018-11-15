Fixed bug in regret computation

functionsTP2_linMAB.py
@@ -85,10 +85,12 @@ def mc_rewards_expectation(rewards_tensor):
 def mc_regret_norm_UCB1(ntrajs, model, T, lamb, alphas, eps=0):
     rewards_tensor, norms_matrix = collect_UCB_trajs(ntrajs, model, T, lamb, alphas, eps)
     mc_expec = mc_rewards_expectation(rewards_tensor)
-    return model.best_arm_reward() - mc_expec, np.mean(norms_matrix, axis=1)
+    best = model.best_arm_reward() * np.arange(1, T + 1)
+    return best - mc_expec, np.mean(norms_matrix, axis=1)
 
 
 def mc_regret_random(ntrajs, model, T):
     rewards_tensor = collect_random_trajs(ntrajs, model, T)
     mc_expec = mc_rewards_expectation(rewards_tensor)
-    return model.best_arm_reward() - mc_expec
+    best = model.best_arm_reward() * np.arange(1, T + 1)
+    return best - mc_expec

mainTP2_linMAB.py
@@ -8,16 +8,16 @@ import functionsTP2_linMAB as funcs_linmab
 importlib.reload(funcs_linmab)
 
 random_state = np.random.randint(0, 375551485)
-model = ToyLinearModel(
-    n_features=8,
-    n_actions=5,
-    random_state=random_state,
-    noise=0.1)
-
-# model = ColdStartMovieLensModel(
+# model = ToyLinearModel(
+#     n_features=8,
+#     n_actions=5,
 #     random_state=random_state,
-#     noise=0.1
-# )
+#     noise=0.1)
+
+model = ColdStartMovieLensModel(
+    random_state=random_state,
+    noise=0.1
+)
 
 # Number of movies
 T = 207
@@ -54,3 +54,4 @@ axes[1].plot(norms_ucb, label="UCB")
 axes[1].plot(norms_greedy, label="Greedy")
 axes[1].set_xlabel("t")
 axes[1].set_ylabel("Distance l2 to true preference vector")
+axes[1].legend()